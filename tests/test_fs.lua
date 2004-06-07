097b7d7e9b058a37f764be82de3542f597b7d7bb
tomas <tomas>
2004-06-07 12:04:10 +0000

    Pequenas correcoes nos arquivos de teste.

--- tests/test_fs.lua
+++ tests/test_fs.lua
@@ -1,10 +1,6 @@
 cgilua.htmlheader ()
 cgilua.put ("<h1>Testing Filesystem library</h1>\n")
-cgilua.put ("<table>\n<tr><td colspan=2>Testing <b>dirlist</b></td></tr>\n")
-dir_t = filesystem.dirlist("/usr/local/cgilua")
-for i = 1, table.getn(dir_t) do
-	cgilua.put ("<tr><td>"..i.."</td><td>"..dir_t[i].."</td></tr>\n")
-end
+cgilua.put ("<table>\n")
 cgilua.put ("<tr><td colspan=2>Testing <b>dir</b></td></tr>\n")
 local i = 0
 for file in filesystem.dir ("/usr/local/cgilua") do
