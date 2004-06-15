8a709c4d58c70d191812223f2ce58344a1145681
tomas <tomas>
2004-06-15 15:26:53 +0000

    Implementacao (e teste) da funcao que calcula o endereco IP remoto.

--- tests/test_main.lua
+++ tests/test_main.lua
@@ -1,4 +1,4 @@
--- $Id: test_main.lua,v 1.1 2004/03/25 19:31:05 tomas Exp $
+-- $Id: test_main.lua,v 1.2 2004/06/15 15:26:53 tomas Exp $
 if ap then handler = ap.handler() end
 cgilua.htmlheader()
 for i,v in pairs (cgi) do
@@ -11,7 +11,14 @@ for i,v in pairs (cgi) do
 	end
 	cgilua.put (string.format ("%s = %s<br>\n", i, tostring(v)))
 end
-cgilua.put"<br>\n"
-cgilua.put("ap="..tostring(ap).."<br>\n")
-cgilua.put("lfcgi="..tostring(lfcgi).."<br>\n")
-if handler then cgilua.put(handler) end
+cgilua.put "<br>\n"
+cgilua.put ("Remote address: "..cgilua.servervariable"REMOTE_ADDR")
+cgilua.put "<br>\n"
+cgilua.put ("ap="..tostring(ap).."<br>\n")
+cgilua.put ("lfcgi="..tostring(lfcgi).."<br>\n")
+if handler then cgilua.put (handler) end
+
+-- Checking Virtual Environment
+local my_output = cgilua.put
+cgilua.put = nil
+assert (cgilua.put == nil, "cannot change cgilua.put value")
