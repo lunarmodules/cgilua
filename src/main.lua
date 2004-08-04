7f080143b5f3bb68e01376ebd19e0833f8a4af59
tomas <tomas>
2004-08-04 12:17:49 +0000

    Troca nos nomes da funcao interna _open para open. Troca no nome da variavel interna libdir para lua_path. Remoca da funcao cgilua.setlibdir antes da execucao do script (ela so' fica acessivel para o arquivo de configuracao). Acrescimo da funcao cgilua.pack, para ser usada no lugar do construtor de tabelas, ja' que este nao guarda todos os valores da lista passada. Modificacao no processo de obtencao do diretorio de bibliotecas (ele passa a ser calculado quando for modificado.

--- src/main.lua
+++ src/main.lua
@@ -2,46 +2,40 @@
 -- Main Lua script.
 -- This script does not depend on the launcher, only on the
 -- basic API.
--- $Id: main.lua,v 1.5 2004/07/29 15:02:49 tomas Exp $
+-- $Id: main.lua,v 1.6 2004/08/04 12:17:49 tomas Exp $
 ---------------------------------------------------------------------
 
 local cgilua_conf = CGILuaRoot().."/conf/cgilua.conf"
 local cgilua_libdir = CGILuaRoot().."/lib/cgilua"
 
----------------------------------------------------------------------
 -- Loading required libraries
----------------------------------------------------------------------
 LUA_PATH = cgilua_libdir.."/?.lua;"..cgilua_libdir.."/?"
 require"cgilua"
 
----------------------------------------------------------------------
+-- Loading/executing configuration file
+cgilua.pcall (cgilua.doif, cgilua_conf)
 -- Cleaning environment
----------------------------------------------------------------------
 cgilua.removeglobals {
 	"os.execute",
 	"loadlib",
+	"cgilua.setlibdir",
 }
-
----------------------------------------------------------------------
--- Executing requested script
----------------------------------------------------------------------
--- Loading configuration file
-cgilua.pcall (cgilua.doif, cgilua_conf)
--- Define directory variables and build `cgi' table.
+-- Defining directory variables and building `cgi' table
 cgi = {}
 cgilua.pcall (cgilua.getparams, cgi)
 -- Changing current directory to the script's "physical" dir
 local curr_dir = lfs.currentdir()
 cgilua.pcall (lfs.chdir, cgilua.script_pdir)
 -- Opening function
-cgilua.pcall (cgilua._open)
+cgilua.pcall (cgilua.open)
 -- Executing script
-local result = { cgilua.pcall (cgilua.handle, cgilua.script_file) }
+local result = cgilua.pack (cgilua.pcall (cgilua.handle, cgilua.script_file))
 -- Closing function
 cgilua.pcall (cgilua.close)
 -- Cleanup
 cgilua.reset ()
+-- Changing to original directory
 cgilua.pcall (lfs.chdir, curr_dir)
-
+-- Returning results to server
 table.remove (result, 1)
 return unpack (result)
