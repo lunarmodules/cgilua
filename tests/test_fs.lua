65775c1cc5d49f6b2b837514d489f8e0ed60be14
tomas <tomas>
2004-08-26 13:49:29 +0000

    Melhorias nos exemplos.

--- tests/test_fs.lua
+++ tests/test_fs.lua
@@ -1,10 +1,23 @@
+function link_dir (dir, base)
+	local path = base.."/"..dir
+	local ok, err = pcall (lfs.dir, path)
+	if ok then
+		return string.format ('<a href="%s">%s</a>',
+			cgilua.mkurlpath ("test_fs.lua", { dir = path }),
+			dir)
+	else
+		return dir
+	end
+end
+
 cgilua.htmlheader ()
 cgilua.put ("<h1>Testing Filesystem library</h1>\n")
 cgilua.put ("<table>\n")
 cgilua.put ("<tr><td colspan=2>Testing <b>dir</b></td></tr>\n")
 local i = 0
-for file in lfs.dir (".") do
+local dir = cgi.dir or "."
+for file in lfs.dir (dir) do
 	i = i+1
-	cgilua.put ("<tr><td>"..i.."</td><td>"..file.."</td></tr>\n")
+	cgilua.put ("<tr><td>"..i.."</td><td>"..link_dir(file, dir).."</td></tr>\n")
 end
 cgilua.put ("</table>\n")
