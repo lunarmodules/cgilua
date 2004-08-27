fec80a2856f07af0c7fb5d58d3797cad6dbce7a3
tomas <tomas>
2004-08-27 16:02:17 +0000

    Organizando os arquivos no CVS.

---
+++ src/cgilua/cookies.lua
@@ -0,0 +1,68 @@
+----------------------------------------------------------------------------
+-- $Id: cookies.lua,v 1.1 2004/08/27 16:02:17 tomas Exp $
+--
+-- Cookies Library
+----------------------------------------------------------------------------
+
+local error = error
+local format, gsub, strfind = string.format, string.gsub, string.find
+local date = os.date
+local escape, unescape = cgilua.escape, cgilua.unescape
+local header, write = HTTP_Response.header, HTTP_Response.write
+local servervariable = HTTP_Request.servervariable
+
+package ("cookies", arg and arg[1])
+
+local function optional (what, name)
+  if name ~= nil and name ~= "" then
+    return format("; %s=%s", what, name)
+  else
+    return ""
+  end
+end
+
+
+local function build (name, value, options)
+  if not name or not value then
+    error("cookie needs a name and a value")
+  end
+  local cookie = name .. "=" .. escape(value)
+  options = options or {}
+  if options.expires then
+    local t = date("!%A, %d-%b-%Y %H:%M:%S GMT", options.expires)
+    cookie = cookie .. optional("expires", t)
+  end
+  cookie = cookie .. optional("path", options.path)
+  cookie = cookie .. optional("domain", options.domain)
+  cookie = cookie .. optional("secure", options.secure)
+  return cookie
+end
+
+
+function set (name, value, options)
+  --cgilua.header("Set-Cookie: "..build(name, value, options).."\n")
+  header("Set-Cookie", build(name, value, options))
+end
+
+
+function sethtml (name, value, options)
+  write(format('<meta http-equiv="Set-Cookie" content="%s">', 
+                build(name, value, options)))
+end
+
+
+function get (name)
+  local cookies = servervariable"HTTP_COOKIE" or ""
+  cookies = ";" .. cookies .. ";"
+  cookies = gsub(cookies, "%s*;%s*", ";")   -- remove extra spaces
+  local pattern = ";" .. name .. "=(.-);"
+  local _, __, value = strfind(cookies, pattern)
+  return value and unescape(value)
+end
+
+
+function delete (name, options)
+  options = options or {}
+  options.expires = 1
+  set(name, "xxx", options)
+end
---
+++ src/cgilua/session.lua
@@ -0,0 +1,146 @@
+----------------------------------------------------------------------------
+-- Session library.
+----------------------------------------------------------------------------
+-- $Id: session.lua,v 1.1 2004/08/27 16:02:17 tomas Exp $
+----------------------------------------------------------------------------
+
+require"lfs"
+
+local assert, loadfile, pairs, type = assert, loadfile, pairs, type
+local format, strfind, strrep, strsub = string.format, string.find, string.rep, string.sub
+local open, write = io.open, io.write
+local remove = os.remove
+local dir = lfs.dir
+
+-- Internal state variables.
+local root_dir = nil
+local counter = 0
+
+package ("session", arg and arg[1])
+
+----------------------------------------------------------------------------
+-- Serializes a session table.
+-- @param tab Table representing the session.
+-- @param outf Function used to generate the output.
+----------------------------------------------------------------------------
+local function serialize_table (tab, outf)
+	outf ("{")
+	for i, v in pairs (tab) do
+		-- serialize the key
+		outf ("[")
+		local t = type(i)
+		if t == "number" or t == "boolean" then
+			outf (i)
+		elseif t == "string" then
+			outf (format ("%q", i))
+		end
+		outf ("]")
+
+		outf ("=")
+		-- serialize the value
+		local t = type (v)
+		local val, fmt
+		if t == "string" then
+			fmt = "%q,"
+			val = v
+		elseif t == "number" then
+			fmt = "%d,"
+			val = v
+		elseif t == "boolean" then
+			fmt = v and "true," or "false,"
+		elseif t == "table" then
+			fmt = ","
+			val = ""
+			serialize_table (v, outf)
+		end
+		outf (format (fmt, val))
+	end
+	outf ("}\n")
+end
+
+----------------------------------------------------------------------------
+-- Creates a new identifier.
+----------------------------------------------------------------------------
+local function new_id ()
+	counter = counter + 1
+	return format ("%08d", counter)
+end
+
+----------------------------------------------------------------------------
+-- Checks identifier format.
+----------------------------------------------------------------------------
+local function check_id (id)
+	return (strfind (id, strrep ("%d", 8)) ~= nil)
+end
+
+----------------------------------------------------------------------------
+-- Produces a file name based on a session.
+-- @param id Session identification.
+-- @return String with the session file name.
+----------------------------------------------------------------------------
+local function filename (id)
+	return root_dir..id..".lua"
+end
+
+----------------------------------------------------------------------------
+-- Deletes a session.
+-- @param id Session identification.
+----------------------------------------------------------------------------
+function delete (id)
+	assert (check_id (id))
+	remove (filename (id))
+end
+
+----------------------------------------------------------------------------
+-- Creates a new session identifier.
+-- @return Session identification.
+----------------------------------------------------------------------------
+function new ()
+	local files = {}
+	for f in dir (root_dir) do
+		files[f] = true
+	end
+	local id = new_id ()
+	while files[id..".lua"] do
+		id = new_id ()
+	end
+	return id
+end
+
+----------------------------------------------------------------------------
+-- Loads data from a session.
+-- @param id Session identification.
+----------------------------------------------------------------------------
+function load (id)
+	assert (check_id (id))
+	local f, err = loadfile (filename (id))
+	if not f then
+		return nil, err
+	else
+		return f()
+	end
+end
+
+----------------------------------------------------------------------------
+-- Saves data to a session.
+-- @param id Session identification.
+-- @param data Table with session data to be saved.
+----------------------------------------------------------------------------
+function save (id, data)
+	assert (check_id (id))
+	local fh = assert (open (filename (id), "w+"))
+	fh:write "return "
+	serialize_table (data, function (s) fh:write(s) end)
+	fh:close()
+end
+
+----------------------------------------------------------------------------
+-- Changes the session directory.
+-- @param path String with the new session directory.
+----------------------------------------------------------------------------
+function setsessiondir (path)
+	if strsub (path, -2, -2) ~= '/' then
+		path = path..'/'
+	end
+	root_dir = path
+end
