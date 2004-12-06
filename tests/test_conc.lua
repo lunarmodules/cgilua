38a91db4b7144ebd8b88e653ea97db66436400f4
tomas <tomas>
2004-12-06 09:18:22 +0000

    Acrescimo da biblioteca "stable".

--- tests/test_conc.lua
+++ tests/test_conc.lua
@@ -1,8 +1,28 @@
 cgilua.htmlheader()
+local pid, ppid = ap.pid ()
+cgilua.put ("pid = "..pid.." ("..ppid..")".."<br>\n")
+
+assert(type(stable.get) == "function")
+assert(type(stable.set) == "function")
+
+local a = stable.get"a" or 0
+cgilua.put ("count = "..a.."<br>\n")
+stable.set ("a", a + 1)
+
+local f = stable.get"f"
+cgilua.put ("f = "..tostring (f).."&nbsp;&nbsp;")
+if not f then
+	local d = os.date()
+	stable.set ("f", function () return d end)
+else
+	cgilua.put ("f() = "..tostring (f ()).."<br>\n")
+end
+
+cgilua.put"<br>\n"
 for i = 1,1000 do
 	cgilua.put (i)
 	for ii = 1,1000 do
-		cgilua.put ("<!>")
+		cgilua.put ("")
 	end
 	cgilua.put ("\n")
 end
