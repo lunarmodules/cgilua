64837881bcd2c9362be8aff4dbfc4bed832e465f
tomas <tomas>
2005-02-11 14:35:01 +0000

    Acrescimo da funcao stable.pairs() que serve para percorrer a tabela estatica stable.

--- tests/test_conc.lua
+++ tests/test_conc.lua
@@ -1,30 +1,37 @@
 cgilua.htmlheader()
 if ap then
 	local pid, ppid = ap.pid ()
-	cgilua.put ("pid = "..pid.." ("..ppid..")".."<br>\n")
+	if not ppid then
+		ppid = "no parent pid"
+	end
+	cgilua.put ("pid = "..pid.." ("..ppid..")".."\n")
 end
 
 assert(type(stable.get) == "function")
 assert(type(stable.set) == "function")
 
-local a = stable.get"a" or 0
-cgilua.put ("count = "..a.."<br>\n")
-stable.set ("a", a + 1)
+cgilua.put"stable.pairs = {<br>\n"
+for i, v in stable.pairs () do
+	cgilua.put (i.." = "..tostring(v).."<br>\n")
+end
+cgilua.put"}<br>\n"
+
+local counter = stable.get"counter" or 0
+stable.set ("counter", counter + 1)
 
 local f = stable.get"f"
-cgilua.put ("f = "..tostring (f).."&nbsp;&nbsp;")
 if not f then
 	local d = os.date()
 	stable.set ("f", function () return d end)
 else
-	cgilua.put ("f() = "..tostring (f ()).."<br>\n")
+	cgilua.put ("f() = "..tostring (f ()))
 end
 
 cgilua.put"<br>\n"
-for i = 1,1000 do
+for i = 1,800 do
 	cgilua.put (i)
 	for ii = 1,1000 do
-		cgilua.put ("")
+		cgilua.put ("<!>")
 	end
 	cgilua.put ("\n")
 end
