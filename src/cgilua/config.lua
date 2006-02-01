010ccb0f0a7887a38cf12c0e510af4d1b96e7f6e
tomas <tomas>
2006-02-01 13:50:36 +0000

    Changing default configuration of sessions library. Adding example of enabling directories for specific applications libraries

--- src/cgilua/config.lua
+++ src/cgilua/config.lua
@@ -9,10 +9,25 @@ end)
 --[[
 require"cgilua.session"
 cgilua.session.setsessiondir ("/tmp/")
-cgilua.addopenfunction (cgilua.session.open)
-cgilua.addclosefunction (cgilua.session.close)
+-- The following function must be called by every script that needs session.
+function cgilua.enablesession ()
+	cgilua.session.open ()
+	cgilua.addclosefunction (cgilua.session.close)
+end
 --]]
 
 -- Compatibility
 cgilua.preprocess = cgilua.handlelp
 cgilua.includehtml = cgilua.lp.include
+
+-- Directories for specific applications libraries.
+-- The following table should be indexed by the virtual path of the application.
+local app_lib_dir = {
+}
+local package = package
+cgilua.addopenfunction (function ()
+	local app = app_lib_dir[cgilua.script_vdir]
+	if app then
+		package.path = package.path..';'..app..'/?.lua'
+	end
+end)
