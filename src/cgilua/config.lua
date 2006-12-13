be405f29881283028b96eaa0cd7162ed404f7b7a
tomas <tomas>
2006-12-13 13:12:38 +0000

    Improving enablesession() to avoid problems with multiple calls. Self-documentation improvements.

--- src/cgilua/config.lua
+++ src/cgilua/config.lua
@@ -1,3 +1,6 @@
+-- CGILua configuration file
+-- $Id: config.lua,v 1.9 2006/12/13 13:12:38 tomas Exp $
+
 -- Emulating old behavior loading file "env.lua" from the script's directory
 --[[
 cgilua.addopenfunction (function ()
@@ -9,7 +12,13 @@ end)
 require"cgilua.session"
 cgilua.session.setsessiondir ("/tmp/")
 -- The following function must be called by every script that needs session.
+local already_enabled = false
 function cgilua.enablesession ()
+	if already_enabled then
+		return
+	else
+		already_enabled = true
+	end
 	cgilua.session.open ()
 	cgilua.addclosefunction (cgilua.session.close)
 end
@@ -18,9 +27,11 @@ end
 cgilua.preprocess = cgilua.handlelp
 cgilua.includehtml = cgilua.lp.include
 
--- Directories for specific applications libraries.
--- The following table should be indexed by the virtual path of the application.
+-- Directories for specific applications' libraries.
+-- The following table should be indexed by the virtual path of the application
+-- and contain the absolute path of the application's Lua-library directory.
 local app_lib_dir = {
+	["/t/"] = "/usr/local/src/cgilua/tests",
 }
 local package = package
 cgilua.addopenfunction (function ()
