5a31d75f0aeeccce6308aea615da5d6c4846acd2
Tomas Guisasola <tomasguisasola@gmail.com>
2013-02-04 21:11:43 -0200

    Corrections on tests (mainly removing previous incompatibilities).

--- tests/test_fs.lua
+++ tests/test_fs.lua
@@ -15,7 +15,7 @@ cgilua.put ("<h1>Testing Filesystem library</h1>\n")
 cgilua.put ("<table>\n")
 cgilua.put ("<tr><td colspan=2>Testing <b>dir</b></td></tr>\n")
 local i = 0
-local dir = cgi.dir or "."
+local dir = cgilua.QUERY.dir or "."
 for file in lfs.dir (dir) do
 	i = i+1
 	cgilua.put ("<tr><td>"..i.."</td><td>"..link_dir(file, dir).."</td></tr>\n")
