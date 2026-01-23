c2eb7e8835d596f274a55f71da6f3b5eb2374439
Tomás Guisasola <tomas@puc-rio.br>
2026-01-22 21:47:15 -0300

    Remotion of unused variable

--- src/cgilua/main.lua
+++ src/cgilua/main.lua
@@ -697,7 +697,6 @@ function cgilua.main (environment, response)
 	assert(response.status, "invalid parameter: response need to have a atribute status")
 
 	-- environment variables
-	_G.CGILUA_APPS = environment.CGILUA_APPS or environment.DOCUMENT_ROOT .. "/cgilua"
 	_G.CGILUA_CONF = environment.CGILUA_CONF or environment.DOCUMENT_ROOT .. "/cgilua"
 	_G.CGILUA_TMP = environment.CGILUA_TMP or os.getenv("TMP") or os.getenv("TEMP") or "/tmp"
 	_G.CGILUA_ISDIRECT = true
